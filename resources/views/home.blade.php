2157cb73c4d8ef6726a3659e9f14539254601867
Wasbis <43838454+Wasbis@users.noreply.github.com>
2022-09-05 23:16:58 +0700

    first and Completed commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="CSS/style.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossorigin="anonymous"
+    />
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet" />
+
+    <!-- monserat font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet" />
+
+    <title>Ngerecokin.id</title>
+  </head>
+  <body>
+    <!--Start Navbar -->
+    <header class="header gradient-nav">
+      <div class="header-inner" style="padding-bottom: 30px">
+        <div class="d-flex justify-content-center container-fluid" style="width: 1200px">
+          <nav class="d-flex justify-content-center navbar navbar-expand-lg bg-nonet">
+            <a class="navbar-brand" href="#"><img src="/Asset/Img/Logo/ngerecokin-white.png" alt="" style="width: 200px; margin-right: 30px" /></a>
+            <div class="container container-nav">
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarScroll"
+                aria-controls="navbarScroll"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarScroll">
+                <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px">
+                  <li class="itemNavnav-item dropdown">
+                    <a class="itemNav nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-current="page" href="#">Home</a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Home 1</a></li>
+                      <li><a class="dropdown-item" href="#">Home 2</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a class="itemNav nav-link" href="#" style="width: 82px">About Us</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="itemNav nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-current="page" href="#">Services</a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Services</a></li>
+                      <li><a class="dropdown-item" href="#">Services Details</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="itemNav nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-current="page" href="#">Projects</a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Projects</a></li>
+                      <li><a class="dropdown-item" href="#">Projects Details</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="itemNav nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-current="page" href="#">Pages</a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">About Us</a></li>
+                      <li><a class="dropdown-item" href="#">Team</a></li>
+                      <li><a class="dropdown-item" href="">Pricing</a></li>
+                      <li><a class="dropdown-item" href="">404 Error</a></li>
+                      <li><a class="dropdown-item" href="">FAQ</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="itemNav nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Blog </a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Blog</a></li>
+                      <li><a class="dropdown-item" href="#">Blog Right Sidebar</a></li>
+                      <li><hr class="dropdown-divider" /></li>
+                      <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a class="itemNav nav-link" href="">Contact</a>
+                  </li>
+                </ul>
+                <form class="mx-5 d-flex" role="search">
+                  <button class="btn btn-outline-success" type="submit" style="width: 160px">Get it Support</button>
+                </form>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+
+      <!-- End Navbar -->
+      <!-- Start Home Area -->
+      <div class="home-area d-flex justify-content-center" style="height: 100%; padding: 20px 0">
+        <div class="d-table d-flex flex-column justify-content-center" style="width: 1200px; height: 100%">
+          <div class="d-table-cell d-flex flex-column justify-content-center">
+            <div class="container d-flex flex-row my-flex-container justify-content-center" style="height: 100%; width: 100%">
+              <div class="container content-left d-flex flex-column justify-content-center" style="width: 50%; color: white; margin-left: 0">
+                <div class="text-left d-flex flex-column justify-content-center">
+                  <h1 style="font-family: 'Montserrat', sans-serif; width: 440px">Content Creator & Digital Agency</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, magna
+                    aliqua. ipsum is simply dummy text of the printing
+                  </p>
+                </div>
+                <div class="button d-flex flex-row justify-content-start align-self-start" style="padding-block: 20px; width: 500px">
+                  <form role="search">
+                    <button class="btn btn-outline-success" type="submit" style="width: 160px">About Us</button>
+                    <button class="btn btn-outline-success" type="submit" style="width: 160px">Contact Us</button>
+                  </form>
+                </div>
+              </div>
+              <div class="content-right d-flex flex-column justify-content-center" style="width: 50%; color: white">
+                <span><img style="width: 98%" src="Asset/img/whatsapp_128px.png" alt="" /></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <!-- End Home Area -->
+    <!-- Start Services Section -->
+    <section class="services-section">
+      <div class="d-flex flex-column justify-content-center">
+        <div class="container d-flex flex-column justify-content-center" style="text-align: center">
+          <span style="font-family: 'Montserrat', sans-serif; color: rgb(186, 72, 72); font-size: 15px"> WHAT WE PROVIDE </span>
+          <h1 style="font-family: 'Fredoka One', cursive">Digital Marketing</h1>
+        </div>
+        <!-- Start card -->
+        <div class="card-container d-flex flex-row justify-content-center">
+          <div class="card d-flex justify-content-center flex-column">
+            <!-- item card -->
+            <div class="card-bg">
+              <img src="/Asset/Img/Card/silvi_belakang.JPG" alt="IT-solution" />
+            </div>
+            <div class="card-context">
+              <div class="dark-bg"></div>
+              <h2>Graphic Design</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt dolore magna aliqua</p>
+              <button>Read More</button>
+            </div>
+          </div>
+          <div class="card d-flex justify-content-center flex-column">
+            <!-- item card -->
+            <div class="card-bg">
+              <img src="/Asset/Img/Card/IMG_9626.JPG" alt="IT-solution" />
+            </div>
+            <div class="card-context">
+              <div class="dark-bg"></div>
+              <h2>Photography</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt dolore magna aliqua</p>
+              <button>Read More</button>
+            </div>
+          </div>
+          <div class="card d-flex justify-content-center flex-column">
+            <!-- item card -->
+            <div class="card-bg">
+              <img src="/Asset/Img/Card/DSC09147.JPG" alt="IT-solution" />
+            </div>
+            <div class="card-context">
+              <div class="dark-bg"></div>
+              <h2>Videography</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt dolore magna aliqua</p>
+              <button>Read More</button>
+            </div>
+          </div>
+        </div>
+
+        <!-- End Card -->
+      </div>
+    </section>
+    <!-- End Services Section -->
+    <!-- Start About Area -->
+    <section class="about-area">
+      <div class="container">
+        <div class="content-area d-flex flex-row justify-content-center">
+          <div class="col-lg-6 col-md-12 col-sm-12">
+            <div class="about-content">
+              <div class="abount-content-text d-flex flex-column justify-content-center">
+                <h6>About Out Company</h6>
+                <h2>Providing your business with a quality IT services is out passion.</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua enim ad
+                  minim veniam, nostrud
+                </p>
+              </div>
+              <div class="container-skill d-flex justify-content-center flex-column">
+                <div class="skill-box d-flex flex-column justify-content-center">
+                  <span class="title">HTML</span>
+                  <div class="skill-bar">
+                    <span class="skill-per">
+                      <span class="tooltip">90%</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+          <div class="col-lg-5">
+            <div class="img ml-5">
+                <img class="ml-5" src="Asset/img/Card/about.jpg" width="400px" alt="">
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End About Area -->
+    <script
+      src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+      integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+      integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
